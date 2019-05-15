b9ce043919c035e1cfde0bc03590cb55604750c0
Travis CI Deployer <deployer@travis-ci.org>
2019-05-15 12:39:49 +0000

    c44e969:	Merge branch 'upgrade'

--- theme/init.js
+++ theme/init.js
@@ -1,8 +1,3 @@
-!function() {
-  var theme, storage = window.localStorage
-  if (storage)
-    theme = storage.theme
-  if (!theme)
-    theme = 'flatly'
-  document.writeln('<link  rel="stylesheet" href="/css/' + theme + '/bootstrap.min.css" />')
-}()
+!function(){var e,t=window.localStorage
+;t&&(e=t.theme),e||(e="flatly"),document.writeln('<link  rel="stylesheet" href="/css/'+e+'/bootstrap.min.css" />')
+}();
