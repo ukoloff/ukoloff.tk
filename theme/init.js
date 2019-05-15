984ee34004a83344328a772cab11aecea3d4251a
Travis CI Deployer <deployer@travis-ci.org>
2019-05-15 12:52:28 +0000

    8087ae4:	Merge branch 'upgrade'

--- theme/init.js
+++ theme/init.js
@@ -1,3 +1,3 @@
 !function(){var e,t=window.localStorage
-;t&&(e=t.theme),e||(e="flatly"),document.writeln('<link  rel="stylesheet" href="/css/'+e+'/bootstrap.min.css" />')
+;t&&(e=t.theme),e||(e="flatly"),document.writeln('<link rel="stylesheet" href="/css/'+e+'/bootstrap.min.css" />')
 }();
