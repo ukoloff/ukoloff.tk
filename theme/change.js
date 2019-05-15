8f841d1240be7527fef76e9b56a44feea766162f
Travis CI Deployer <deployer@travis-ci.org>
2019-05-15 09:53:31 +0000

    ddc8952:	Compress with Terser

--- theme/change.js
+++ theme/change.js
@@ -1,57 +1,11 @@
-!function () {
-  var data = theme()
-  inject()
-
-  function theme() {
-    var links = document.getElementsByTagName('link')
-    for (var i = links.length - 1; i >= 0; --i) {
-      var link = links[i]
-      if (link.rel != 'stylesheet') continue;
-      var re = /.*\/(.*?)\//.exec(link.href)
-      if (!re) continue
-      return {
-        link: link,
-        theme: re[1]
-      }
-    }
-    return {}
-  }
-
-  function inject() {
-    var form = document.forms
-    form = form[form.length - 1]
-    form.onsubmit = submit
-    for (var i = form.length - 1; i >=0; --i) {
-      var input = form[i]
-      switch (input.type) {
-        case 'button':
-          input.onclick = back
-          break
-        case 'radio':
-          input.onclick = click
-          if (input.value == data.theme) {
-            input.checked = true
-            input.focus()
-          }
-      }
-    }
-  }
-
-  function submit() {
-    var storage = window.localStorage
-    if (storage)
-      storage.theme = data.theme
-    back()
-    return false
-  }
-
-  function back() {
-    history.back()
-  }
-
-  function click() {
-    data.theme = this.value
-    if (!data.link) return
-    data.link.href = data.link.href.replace(/(.*)\/.*?\//, '$1/' + data.theme + '/')
-  }
-}()
+!function(){var e=function(){
+for(var e=document.getElementsByTagName("link"),t=e.length-1;t>=0;--t){
+var n=e[t];if("stylesheet"==n.rel){var r=/.*\/(.*?)\//.exec(n.href)
+;if(r)return{link:n,theme:r[1]}}}return{}}();function t(){
+var t=window.localStorage;return t&&(t.theme=e.theme),n(),!1}
+function n(){history.back()}function r(){
+e.theme=this.value,e.link&&(e.link.href=e.link.href.replace(/(.*)\/.*?\//,"$1/"+e.theme+"/"))
+}!function(){var c=document.forms;(c=c[c.length-1]).onsubmit=t
+;for(var i=c.length-1;i>=0;--i){var o=c[i];switch(o.type){case"button":
+o.onclick=n;break;case"radio":
+o.onclick=r,o.value==e.theme&&(o.checked=!0,o.focus())}}}()}();
