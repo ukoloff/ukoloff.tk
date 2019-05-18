e683344501b3026795e655a7c490cd8c8623f3f6
Travis CI Deployer <deployer@travis-ci.org>
2019-05-18 16:38:50 +0000

    f8bdbec:	Use backcalls

--- theme/change.js
+++ theme/change.js
@@ -1,12 +1,13 @@
 !function(){var e,t;function n(){var t
 ;return null!=(t=window.localStorage)&&(t.theme=e.theme),o(),!1}
-function o(){history.back()}function i(){var n,o,i;function c(){
+function o(){history.back()}function i(){var n,o,i
+;(n=this.value)!==e.theme&&e.link&&(e.theme=n,
+(t||setTimeout)(function(){
+i.transition="opacity 120ms",i.opacity=0,setTimeout(function(){
 e.link.href=e.link.href.replace(/(.*)\/.*?\//,"$1/"+e.theme+"/"),
-setTimeout(u,210)}function u(){i.opacity=1,setTimeout(r,120)}
-function r(){o&&e.theme!==n?o():t=void 0}
-(n=this.value)!==e.theme&&e.link&&(e.theme=n,(t||setTimeout)(function(){
-i.transition="opacity 120ms",i.opacity=0,setTimeout(c,120)
-}),t=function(e){e},i=document.body.style)}e=function(){var e,t,n,o
+setTimeout(function(){i.opacity=1,setTimeout(function(){
+o&&e.theme!==n?o():t=void 0},120)},210)},120)}),t=function(e){e
+},i=document.body.style)}e=function(){var e,t,n,o
 ;for(e=document.getElementsByTagName("link"),
 t=e.length-1;t>=0;--t)if("stylesheet"===(n=e[t]).rel&&(o=/.*\/(.*?)\//.exec(n.href)))return{
 link:n,theme:o[1]};return{}}(),function(){var t,c,u,r,a
