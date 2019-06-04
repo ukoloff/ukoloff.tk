21a211e644e88a5f7550985bfb12b148641bba3c
Travis CI Deployer <deployer@travis-ci.org>
2019-06-04 10:07:34 +0000

    a346043:	Merge branch 'gulp'

--- theme/change.js
+++ theme/change.js
@@ -1,17 +1,16 @@
-!function(){var e,t;function n(){var t
-;return null!=(t=window.localStorage)&&(t.theme=e.theme),o(),!1}
-function o(){history.back()}function i(){var n,o,i
-;(n=this.value)!==e.theme&&e.link&&(e.theme=n,
-(t||setTimeout)(function(){
-i.transition="opacity 120ms",i.opacity=0,setTimeout(function(){
-e.link.href=e.link.href.replace(/(.*)\/.*?\//,"$1/"+e.theme+"/"),
-setTimeout(function(){i.opacity=1,setTimeout(function(){
-o&&e.theme!==n?o():t=void 0},120)},210)},120)}),t=function(e){e
-},i=document.body.style)}e=function(){var e,t,n,o
+!function(){var e,t,n;function o(){var e
+;return null!=(e=window.localStorage)&&(e.theme=t.theme),i(),!1}
+function i(){history.back()}function c(){var o,i,c
+;(o=this.value)!==t.theme&&t.link&&(t.theme=o,(n||e)(function(){
+c.transition="opacity 120ms",c.opacity=0,e(function(){
+t.link.href=t.link.href.replace(/(.*)\/.*?\//,"$1/"+t.theme+"/"),
+e(function(){c.opacity=1,e(function(){i&&t.theme!==o?i():n=void 0},120)
+},210)},120)}),n=function(e){e},c=document.body.style)}
+e=setTimeout,t=function(){var e,t,n,o
 ;for(e=document.getElementsByTagName("link"),
 t=e.length-1;t>=0;--t)if("stylesheet"===(n=e[t]).rel&&(o=/.*\/(.*?)\//.exec(n.href)))return{
-link:n,theme:o[1]};return{}}(),function(){var t,c,u,r,a
-;for((t=(c=document.forms)[c.length-1]).onsubmit=n,
-u=0,r=t.length;u<r;++u)switch((a=t[u]).type){case"button":a.onclick=o
+link:n,theme:o[1]};return{}}(),function(){var e,n,r,u,a
+;for((e=(n=document.forms)[n.length-1]).onsubmit=o,
+r=0,u=e.length;r<u;++r)switch((a=e[r]).type){case"button":a.onclick=i
 ;break;case"radio":
-a.onclick=i,a.value===e.theme&&(a.checked=!0,a.focus())}}()}();
+a.onclick=c,a.value===t.theme&&(a.checked=!0,a.focus())}}()}();
