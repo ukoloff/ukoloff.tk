db93ad551c98c5a222cd4983cdfa3a9aa1c74198
GitHub Pages Publisher <none>
2020-04-13 13:43:24 +0000

    97b4109: Try GitHub Actions

--- theme/change.js
+++ theme/change.js
@@ -1,14 +1,13 @@
 !function(){var e,t,n;function o(){var e
 ;return null!=(e=window.localStorage)&&(e.theme=t.theme),i(),!1}
-function i(){history.back()}function c(){var o,i,c
+function i(){history.back()}function c(){var o,i
 ;(o=this.value)!==t.theme&&t.link&&(t.theme=o,(n||e)((function(){
-c.transition="opacity 120ms",c.opacity=0,e((function(){
+i.transition="opacity 120ms",i.opacity=0,e((function(){
 t.link.href=t.link.href.replace(/(.*)\/.*?\//,"$1/"+t.theme+"/"),
-e((function(){c.opacity=1,e((function(){i&&t.theme!==o?i():n=void 0
-}),120)}),210)}),120)})),n=function(e){e},c=document.body.style)}
-e=setTimeout,t=function(){var e,t,n,o
-;for(e=document.getElementsByTagName("link"),
-t=e.length-1;t>=0;--t)if("stylesheet"===(n=e[t]).rel&&(o=/.*\/(.*?)\//.exec(n.href)))return{
+e((function(){i.opacity=1,e((function(){n=void 0}),120)}),210)}),120)
+})),n=function(e){e},i=document.body.style)}e=setTimeout,t=function(){
+var e,t,n,o
+;for(e=document.getElementsByTagName("link"),t=e.length-1;t>=0;--t)if("stylesheet"===(n=e[t]).rel&&(o=/.*\/(.*?)\//.exec(n.href)))return{
 link:n,theme:o[1]};return{}}(),function(){var e,n,r,u,a
 ;for((e=(n=document.forms)[n.length-1]).onsubmit=o,
 r=0,u=e.length;r<u;++r)switch((a=e[r]).type){case"button":a.onclick=i
